169d8e477468383a5191c084da173244454fca0f
Jevon Hill <1jevonhill@gmail.com>
2020-01-21 00:22:32 -0500

    added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  get  'main/index'
   get 'affirmations/index'
+  get 'motivations/index'
+  get 'meditations/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :affirmations
-
-  root 'affirmations#index'
+  resources :main, only: [:index] do
+  resources :affirmations, only: [:index, :show, :new, :update, :create, :update, :destroy]
+  resources :motivations, only: [:index, :show] 
+    resources :meditations, only: [:index, :show]
+  end
+ 
+  resources :video, only: [:index, :show]
+  resources :audio, only: [:index, :show]
+  root 'main#index'
 end
